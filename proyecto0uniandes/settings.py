254924bb09e592b26ea6b63ba61c7d1e4ed0825e
Daniel Jaramillo <nanodan47@gmail.com>
2020-08-12 17:49:09 -0500

    actualizacion settings.py

--- proyecto0uniandes/settings.py
+++ proyecto0uniandes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8j#hd8(*49f_#(p61bs(dg&f_$e&i$zvtre@bb41j_!yaepb+!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
